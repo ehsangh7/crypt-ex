4b978c203c075cbcb3867ca3c8214270c2f1893e
ehsangh7 <[ehsanghochagh@gmail.com]>
2021-11-16 13:06:59 +0330

    modifeid

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -15,7 +15,7 @@ const { Title } = Typography;
 const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data;
-  console.log(globalState)
+  console.log(globalStats)
   if (isFetching) return <Loader />;
 
   return (
